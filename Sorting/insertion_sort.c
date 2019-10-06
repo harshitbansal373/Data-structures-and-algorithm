d4d717ed85e15cb0f75ade3af2d0ca68186402d7
Harshit Bansal <harshitbansal373@gmail.com>
2019-10-06 10:26:04 +0530

    insertion_sort

---
+++ Sorting/insertion_sort.c
@@ -0,0 +1,34 @@
+// insertion sort
+
+#include<stdio.h>
+int main(){
+    int i,n,ptr,temp,value;
+    printf("enter number of number elements in array\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("\nenter array elements");
+    for(i=0;i<n;i++){
+        scanf("%d",&value);
+        arr[i]=value;
+    }
+    printf("\narray elements\n");
+    for(i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+    
+    for(i=1;i<n;i++){
+        temp=arr[i];
+        ptr=i-1;
+        while(ptr>=0 && temp<arr[ptr]){
+            arr[ptr+1]=arr[ptr];
+            ptr=ptr-1;
+        }
+        arr[ptr+1]=temp;
+    } 
+    
+    printf("\ninsertion sorted array\n");
+    for(i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
